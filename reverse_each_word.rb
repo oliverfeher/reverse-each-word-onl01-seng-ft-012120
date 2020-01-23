48f0461a32d62924c000fc5d39ce74004ea5ffa2
Oliver Feher <olcsee@gmail.com>
2020-01-23 06:16:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(string)
-  string2 = ""
-  string.each {|e| string2.push(e.reverse)
+  string2 = []
+  string.split(" ").each { |e| string2.push(e.reverse)}
+  return string2.join(" ")
 end
   
