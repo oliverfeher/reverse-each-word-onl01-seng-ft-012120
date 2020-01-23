a49c30beb812a805385d2fbbf655670e4b0e7186
Oliver Feher <olcsee@gmail.com>
2020-01-23 06:18:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
+#converting string to array with using split to be able to use each-method
+#itirate over all the words and reverse them, then push them into string2 array
+#convert array into string and return the value of string2
 
 
-
- reverse_each_word(string)
+def reverse_each_word(string)
   string2 = []
   string.split(" ").each { |e| string2.push(e.reverse)}
   return string2.join(" ")
